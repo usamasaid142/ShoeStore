0b601d320164b4499187d13c5e861dd3f80d8104
osama <usamasaid142@gmail.com>
2022-08-21 10:38:48 +0200

    add shoe detail

---
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -0,0 +1,27 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.udacity.shoestore.databinding.ShoeDetailfragmentBinding
+
+
+class ShoeDetailFragment : Fragment() {
+
+    private lateinit var binding:ShoeDetailfragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding= ShoeDetailfragmentBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
