73ca5ebab47b090f166d690aae722b31c870652b
osama <usamasaid142@gmail.com>
2022-08-21 12:39:46 +0200

    add shoe detail

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,17 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.ui.AppBarConfiguration
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+
+
     }
 }
--- app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.ShoeDetailfragmentBinding
+import com.udacity.shoestore.models.Shoe
 
 
 class ShoeDetailFragment : Fragment() {
@@ -22,6 +25,36 @@ class ShoeDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initbutton()
+        getshoeinfodetail()
     }
+
+
+    fun initbutton(){
+        binding.btnCancel.setOnClickListener {
+
+            //findNavController().navigate()
+        }
+        binding.btnSave.setOnClickListener {
+
+           // val action =
+
+        }
+    }
+
+    fun getshoeinfodetail():List<Shoe>{
+       val shoelist= mutableListOf<Shoe>()
+        if(binding.etShoeName.text.toString().isNullOrEmpty()&& binding.etSize.text.toString() as Double!=null&&
+        binding.etCompany.text.toString().isNullOrEmpty()&&binding.etDesc.text.toString().isNullOrEmpty()){
+            val shoe=Shoe(binding.etShoeName.text.toString(),binding.etSize.text.toString() as Double,
+                binding.etCompany.text.toString(),binding.etDesc.text.toString())
+            shoelist.add(shoe)
+        }else{
+            Toast.makeText(requireContext(),"all fields required",Toast.LENGTH_LONG).show()
+        }
+
+      return shoelist
+    }
+
 
 }
